1ab5911741106fff425882f8f113d33455427634
Dani Latorre <dani@danilat.com>
2017-12-09 01:28:59 +0100

    test for Error404 component

---
+++ tests/Error404.spec.js
@@ -0,0 +1,13 @@
+
+import { mount } from 'vue-test-utils'
+import Error404 from '@/components/Error404'
+import { QBtn } from 'quasar'
+import Vue from 'vue'
+
+describe('Error404', () => {
+  it('has a go home button', async () => {
+    Vue.prototype.$q = {theme: 'mat'}
+    const wrapper = mount(Error404)
+    expect(wrapper.find(QBtn).text()).toContain('Go home')
+  })
+})
