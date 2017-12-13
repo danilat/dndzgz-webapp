cba25839ab7215565134c7458b176adb42d76b02
Dani Latorre <dani@danilat.com>
2017-12-13 16:02:43 +0100

    added infowindow to map markers

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -8,13 +8,17 @@
   >
       <gmap-marker
         :key="marker.id"
-        v-for="marker in markers"
+        v-for="(marker, index) in markers"
         :position="marker"
         :clickable="true"
         :title="marker.title"
-        @click="center=marker"
+        @click="showMarkerInfo(index)"
         icon="http://www.dndzgz.com/cache/markers/marker-bus.png"
       ></gmap-marker>
+
+      <gmap-info-window :position="infoWindow.position" :opened="infoWindow.opened" @closeclick="closeInfoWindow()" :options="infoWindow.options">
+        {{infoWindow.content}}
+      </gmap-info-window>
 
       <gmap-marker
         v-show="currentPosition"
@@ -38,12 +42,32 @@ export default {
     return {
       center: {lat: 41.641184, lng: -0.894032},
       currentPosition: null,
-      markers: null
+      markers: null,
+      infoWindow: {
+        opened: true,
+        options: {
+          pixelOffset: {
+            width: 0,
+            height: -35
+          }
+        }
+      }
     }
   },
   async created () {
     this.markers = await retrieveAllBusStops()
     this.center = this.currentPosition = await userCurrentPosition()
+  },
+  methods: {
+    showMarkerInfo (index) {
+      const selected = this.markers[index]
+      this.infoWindow.position = this.center = selected
+      this.infoWindow.opened = true
+      this.infoWindow.content = selected.title
+    },
+    closeInfoWindow () {
+      this.infoWindow.opened = false
+    }
   }
 }
 </script>
