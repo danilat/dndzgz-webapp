ac42de4876acfdee8c89a2e1c13dc4ec5b7ca6b7
Dani Latorre <dani@danilat.com>
2017-12-13 19:27:51 +0100

    little readability improvementes

--- tests/pages/Map.spec.js
+++ tests/pages/Map.spec.js
@@ -13,23 +13,23 @@ describe('Map', () => {
     firstMarker = markers[0]
   })
 
-  it('has the bus stops', async () => {
-    expect(wrapper.html()).toContain('CALLE CANTANDO BAJO LA LLUVIA')
+  it('has the bus stops', () => {
+    expect(wrapper.html()).toContain('GOYA')
   })
 
-  it('shows the marker title in the infowindow when a marker is clicked', () => {
+  it('shows the marker title in the infowindow when is clicked', () => {
     wrapper.find('#marker'+firstMarker.id).trigger('click')
 
     expect(wrapper.find('#infoWindow').text()).toContain(firstMarker.title)
   })
 
-  it('click on infowindow button navigates bus detail', async () => {
-    const navigateToMapSpy = jest.fn()
-    wrapper.vm.dndzgzRouter = {navigateToBusDetail: navigateToMapSpy}
-
+  it('click on infowindow button navigates bus detail', () => {
+    const navigationSpy = jest.fn()
+    wrapper.vm.dndzgzRouter = {navigateToBusDetail: navigationSpy}
     wrapper.find('#marker'+firstMarker.id).trigger('click')
+
     wrapper.find('#infoWindow').find('button').trigger('click')
 
-    expect(navigateToMapSpy).toHaveBeenCalledWith(firstMarker.id)
+    expect(navigationSpy).toHaveBeenCalledWith(firstMarker.id)
   })
 })
