040f677454d8c43d7623d26b90b1b72cb469cf81
Dani Latorre <dani@danilat.com>
2017-12-14 14:50:41 +0100

    added toolbar to the ServiceList

--- src/pages/ServiceList.vue
+++ src/pages/ServiceList.vue
@@ -1,6 +1,12 @@
 <template>
   <!-- if you want automatic padding use "layout-padding" class -->
   <div class="layout-padding">
+    <q-toolbar color="primary">
+      <q-toolbar-title>
+        DNDzgz
+      </q-toolbar-title>
+    </q-toolbar>
+
     <q-list highlight>
       <q-item @click="goToBusMap()" id="busOption">
         <q-item-side avatar="http://www.dndzgz.com/cache/markers/marker-bus.png"/>
@@ -16,11 +22,22 @@
 </template>
 
 <script>
-import {QList, QListHeader, QItem, QItemSide, QItemMain, QItemTile} from 'quasar'
+import {
+  QToolbar,
+  QToolbarTitle,
+  QList,
+  QListHeader,
+  QItem,
+  QItemSide,
+  QItemMain,
+  QItemTile
+} from 'quasar'
 import {DndZgzRouter} from '../core/router'
 
 export default {
   components: {
+    QToolbar,
+    QToolbarTitle,
     QList,
     QListHeader,
     QItem,
