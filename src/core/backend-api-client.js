424352acd33df86d42f69d78a0de276ff174e3b2
Dani Latorre <dani@danilat.com>
2017-12-18 13:31:46 +0100

    now the API endpoint uses https

--- src/core/backend-api-client.js
+++ src/core/backend-api-client.js
@@ -1,7 +1,7 @@
 import fetchJsonp from 'fetch-jsonp'
 
 export const backendApiClient = (service, id = '') => {
-  return fetchJsonp(`http://api.dndzgz.com/services/${service}/${id}`)
+  return fetchJsonp(`https://dndzgz.herokuapp.com/services/${service}/${id}`)
     .then((response) => {
       return response.json()
     })
