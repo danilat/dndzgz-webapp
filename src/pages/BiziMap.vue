6ecf260e2a3a7c0bc0ef9e791186acd3ccb05051
Dani Latorre <dani@danilat.com>
2017-12-19 17:34:10 +0100

    added infowindow format to the Bizi Map

--- src/pages/BiziMap.vue
+++ src/pages/BiziMap.vue
@@ -8,7 +8,7 @@
         Bizi
       </q-toolbar-title>
     </q-toolbar>
-    <map-with-markers icon="statics/marker-bizi.png" :markers="stations"></map-with-markers>
+    <map-with-markers icon="statics/marker-bizi.png" :markers="stations" :infoWindowContentFormatter="infoWindowContentFormatter"></map-with-markers>
   </div>
 </template>
 
@@ -42,6 +42,9 @@ export default {
   methods: {
     goBack () {
       this.dndzgzRouter.navigateToServiceList()
+    },
+    infoWindowContentFormatter (station) {
+      return station.title
     }
   }
 }
