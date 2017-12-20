db146f6e102628932bed32550ca7573ad5763227
Dani Latorre <dani@danilat.com>
2017-12-20 01:04:31 +0100

    added results cache when the commands get the collections

--- src/core/commands.js
+++ src/core/commands.js
@@ -1,25 +1,15 @@
 import { backendApiClient } from './backend-api-client'
 
 export const retrieveAllBusStops = async (apiClient = backendApiClient) => {
-  const response = await apiClient('bus')
-  return responseWithLocationsMapper(response)
+  return retreiveUnlessIsCached('bus', apiClient)
 }
 
 export const retrieveAllBiziStations = async (apiClient = backendApiClient) => {
-  const response = await apiClient('bizi')
-  return responseWithLocationsMapper(response)
+  return retreiveUnlessIsCached('bizi', apiClient)
 }
 
 export const retrieveAllTaxiStops = async (apiClient = backendApiClient) => {
-  const response = await apiClient('taxis')
-  return responseWithLocationsMapper(response)
-}
-
-const responseWithLocationsMapper = (response) => {
-  return response.locations.map((location) => {
-    location.lng = location.lon
-    return location
-  })
+  return retreiveUnlessIsCached('taxis', apiClient)
 }
 
 export const retrieveBusStopEstimation = (id, apiClient = backendApiClient) => {
@@ -28,4 +18,21 @@ export const retrieveBusStopEstimation = (id, apiClient = backendApiClient) => {
 
 export const retrieveBiziStationEstimation = (id, apiClient = backendApiClient) => {
   return apiClient('bizi', id)
+}
+
+const cachedCollections = {}
+const retreiveUnlessIsCached = async (service, apiClient) => {
+  const isNotCached = !cachedCollections[service]
+  if (isNotCached) {
+    const response = await apiClient(service)
+    cachedCollections[service] = responseWithLocationsMapper(response)
+  }
+  return cachedCollections[service]
+}
+
+const responseWithLocationsMapper = (response) => {
+  return response.locations.map((location) => {
+    location.lng = location.lon
+    return location
+  })
 }
