38e07b6104a5aab709edb1c46e38237967ca0b4a
Dani Latorre <dani@danilat.com>
2017-12-20 18:35:26 +0100

    added a MapHeader component

---
+++ tests/components/MapHeader.spec.js
@@ -0,0 +1,27 @@
+import { mount } from 'vue-test-utils'
+import MapHeader from '@/components/MapHeader'
+
+describe('MapHeader', () => {
+  const aTitle = 'Irrelevant title'
+  let wrapper
+  beforeEach(() => {
+    wrapper = mount(MapHeader, {
+      propsData: {
+        title: aTitle,
+      }
+    })
+  })
+
+  it('has the title', () => {
+    expect(wrapper.find('#header_title').text()).toBe(aTitle)
+  })
+
+  it('click on back goes to service list', () => {
+    const navigationSpy = jest.fn()
+    wrapper.vm.dndzgzRouter = {navigateToServiceList: navigationSpy}
+
+    wrapper.find('#back').trigger('click')
+
+    expect(navigationSpy).toHaveBeenCalled()
+  })
+})
