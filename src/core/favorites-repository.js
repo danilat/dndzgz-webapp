93fbe9b8fd0f3e7563c060358d765c53eff71902
Dani Latorre <dani@danilat.com>
2018-03-15 21:51:20 +0100

    alled retrieve all the favorites in the repository

--- src/core/favorites-repository.js
+++ src/core/favorites-repository.js
@@ -1,9 +1,12 @@
 const collection = {}
-export const favorites = {
+export const favoritesRepository = {
   put(favorite){
     collection[favorite.id] = favorite
   },
   get(id){
     return collection[id]
+  },
+  all(){
+    return Object.values(collection)
   }
 }
--- tests/core/favorites-repository.spec.js
+++ tests/core/favorites-repository.spec.js
@@ -1,12 +1,22 @@
-import {favorites} from '@/core/favorites-repository'
+import {favoritesRepository} from '@/core/favorites-repository'
 
 describe('Favorites Repository', () => {
   it('puts a favorite', async() => {
     const aFavorite = {id: 'a fav'}
-    await favorites.put(aFavorite)
+    await favoritesRepository.put(aFavorite)
 
-    const favorite = await favorites.get(aFavorite.id)
+    const favorite = await favoritesRepository.get(aFavorite.id)
 
     expect(favorite).toEqual(aFavorite)
+  })
+  it('get all the favorites', async() => {
+    const aFavorite = {id: 'a fav'}
+    const anotherFavorite = {id: 'another fav'}
+    await favoritesRepository.put(aFavorite)
+    await favoritesRepository.put(anotherFavorite)
+
+    const favorites = await favoritesRepository.all()
+
+    expect(favorites.length).toEqual(2)
   })
 })
