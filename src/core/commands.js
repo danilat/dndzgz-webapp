7919dfcca9231841d0523f5f345ec4726bd3ddd1
Dani Latorre <dani@danilat.com>
2018-03-19 14:03:49 +0100

    we need to store the type for a favorite

--- src/core/commands.js
+++ src/core/commands.js
@@ -28,14 +28,17 @@ export const retrieveTramStopEstimation = (id, apiClient = backendApiClient) => 
   return apiClient('tram', id)
 }
 
-export const markAsFavorite = (name, point, favoritesRepository) => {
+export const markAsFavorite = (name, point, type, favoritesRepository) => {
   if (!name) {
     throw new Error('name is required')
   }
   if (!point) {
     throw new Error('point is required')
   }
-  const favorite = Object.assign({name: name}, point)
+  if (!type) {
+    throw new Error('type is required')
+  }
+  const favorite = Object.assign({name: name, type: type}, point)
   return favoritesRepository.put(favorite)
 }
 
--- tests/core/commands/mark_as_favorite.spec.js
+++ tests/core/commands/mark_as_favorite.spec.js
@@ -1,12 +1,11 @@
 import {markAsFavorite} from '@/core/commands'
 
 describe('Mark as Favorite', () => {
-  let aPoint
-  let aName
+  const aPoint = {title: 'a title', id: 'an id', lng: 'a lng', lat:'a lat'}
+  const aName = 'a name'
   let spiedRepository
+  const aType = 'a_type'
   beforeEach(() => {
-    aPoint = {title: 'a title', id: 'an id', lng: 'a lng', lat:'a lat'}
-    aName = 'a name'
     spiedRepository = {
       put: jest.fn(favorite => favorite)
     }
@@ -14,19 +13,24 @@ describe('Mark as Favorite', () => {
 
   describe('with a name and a point', () =>{
     it('has the name', async () => {
-      const favorite = await markAsFavorite(aName, aPoint, spiedRepository)
+      const favorite = await markAsFavorite(aName, aPoint, aType, spiedRepository)
 
       expect(favorite.name).toEqual(aName)
     })
+    it('has the type of point', async () => {
+      const favorite = await markAsFavorite(aName, aPoint, aType, spiedRepository)
+
+      expect(favorite.type).toEqual(aType)
+    })
     it('has the point', async () => {
-      const favorite = await markAsFavorite(aName, aPoint, spiedRepository)
+      const favorite = await markAsFavorite(aName, aPoint, aType, spiedRepository)
 
       expect(favorite.id).toEqual(aPoint.id)
       expect(favorite.lng).toEqual(aPoint.lng)
       expect(favorite.lat).toEqual(aPoint.lat)
     })
     it('the favorite is stored', async() =>{
-      const favorite = await markAsFavorite(aName, aPoint, spiedRepository)
+      const favorite = await markAsFavorite(aName, aPoint, aType, spiedRepository)
 
       expect(spiedRepository.put).toBeCalledWith(favorite)
     })
@@ -34,16 +38,25 @@ describe('Mark as Favorite', () => {
   it('fails without name', async () =>{
     expect.assertions(2)
     try {
-      await markAsFavorite("", aPoint, spiedRepository)
+      await markAsFavorite("", aPoint, aType, spiedRepository)
     } catch(error){
       expect(error).not.toBeNull()
       expect(error.message).toEqual("name is required")
     }
   })
+  it('fails without type', async () =>{
+    expect.assertions(2)
+    try {
+      await markAsFavorite(aName, aPoint, "", spiedRepository)
+    } catch(error){
+      expect(error).not.toBeNull()
+      expect(error.message).toEqual("type is required")
+    }
+  })
   it('fails without point', async () =>{
     expect.assertions(2)
     try {
-      await markAsFavorite(aName, null, spiedRepository)
+      await markAsFavorite(aName, null, aType, spiedRepository)
     } catch(error){
       expect(error).not.toBeNull()
       expect(error.message).toEqual("point is required")
