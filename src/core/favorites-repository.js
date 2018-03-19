0e8c62d025f556149bb1ef6f0c424991098bdc78
Dani Latorre <dani@danilat.com>
2018-03-19 21:02:18 +0100

    now the favorites repository is also implemented for localstorage

--- src/core/favorites-repository.js
+++ src/core/favorites-repository.js
@@ -1,5 +1,5 @@
 const collection = {}
-export const favoritesRepository = {
+export const inMemoryFavoritesRepository = {
   put (favorite) {
     collection[favorite.id] = favorite
   },
@@ -10,3 +10,29 @@ export const favoritesRepository = {
     return Object.values(collection)
   }
 }
+
+if (typeof window.localStorage === 'undefined' || window.localStorage === null) {
+  window.localStorage = require('localStorage')
+}
+
+export const localStorageFavoritesRepository = {
+  put (favorite) {
+    const collection = this.storedFavorites()
+    collection[favorite.id] = favorite
+    localStorage.setItem('favorites', JSON.stringify(collection))
+  },
+  get (id) {
+    return this.storedFavorites()[id]
+  },
+  all () {
+    return Object.values(this.storedFavorites())
+  },
+  storedFavorites () {
+    let collection = JSON.parse(localStorage.getItem('favorites'))
+    if (!collection) {
+      collection = {}
+    }
+    return collection
+  }
+}
+export const favoritesRepository = localStorageFavoritesRepository
--- tests/core/favorites-repository.spec.js
+++ tests/core/favorites-repository.spec.js
@@ -1,24 +1,31 @@
-import {favoritesRepository} from '@/core/favorites-repository'
+import {inMemoryFavoritesRepository, localStorageFavoritesRepository} from '@/core/favorites-repository'
 
-describe('Favorites Repository', () => {
-  it('puts a favorite', async() => {
-    const aFavorite = {id: 'a fav'}
-    await favoritesRepository.put(aFavorite)
-
-    const favorite = await favoritesRepository.get(aFavorite.id)
-
-    expect(favorite).toEqual(aFavorite)
-  })
-  it('get all the favorites', async() => {
-    const aFavorite = {id: 'a fav'}
-    const anotherFavorite = {id: 'another fav'}
-    await favoritesRepository.put(aFavorite)
-    await favoritesRepository.put(anotherFavorite)
-
-    const favorites = await favoritesRepository.all()
-
-    expect(favorites.length).toEqual(2)
-    expect(favorites).toContain(aFavorite)
-    expect(favorites).toContain(anotherFavorite)
-  })
+describe('In Memory Favorites Repository', () => {
+  favoritesRepositoryContractTests(inMemoryFavoritesRepository)
 })
+describe('Local Storage Favorites Repository', () => {
+  favoritesRepositoryContractTests(localStorageFavoritesRepository)
+})
+
+function favoritesRepositoryContractTests(favoritesRepository){
+    it('puts a favorite', async() => {
+      const aFavorite = {id: 'a fav'}
+      await favoritesRepository.put(aFavorite)
+
+      const favorite = await favoritesRepository.get(aFavorite.id)
+
+      expect(favorite).toEqual(aFavorite)
+    })
+    it('get all the favorites', async() => {
+      const aFavorite = {id: 'a fav'}
+      const anotherFavorite = {id: 'another fav'}
+      await favoritesRepository.put(aFavorite)
+      await favoritesRepository.put(anotherFavorite)
+
+      const favorites = await favoritesRepository.all()
+
+      expect(favorites.length).toEqual(2)
+      expect(favorites).toContainEqual(aFavorite)
+      expect(favorites).toContainEqual(anotherFavorite)
+    })
+}
