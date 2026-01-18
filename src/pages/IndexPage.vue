c2e1ca8b9ce6e37a7778fded16f3ed4baa1a9fcb
Dani Latorre <dani@danilat.com>
2026-01-18 12:18:16 +0100

    Adding the logo to the header of the page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,8 @@
 <template>
   <q-page class="column">
     <q-toolbar class="bg-primary text-white">
-      <q-toolbar-title>
-        DNDzgz
-      </q-toolbar-title>
+      <img src="logo.png" alt="DNDzgz" width="32" height="32" class="q-mr-sm" />
+      <q-toolbar-title> DNDzgz </q-toolbar-title>
     </q-toolbar>
 
     <q-list separator>
@@ -19,44 +18,44 @@
 
       <q-item clickable v-ripple to="/bizi" id="biziOption">
         <q-item-section avatar>
-           <img src="marker-bizi.png" width="32" height="32" />
+          <img src="marker-bizi.png" width="32" height="32" />
         </q-item-section>
         <q-item-section>Bizi</q-item-section>
-         <q-item-section side>
+        <q-item-section side>
           <q-icon name="arrow_forward" color="grey" />
         </q-item-section>
       </q-item>
 
       <q-item clickable v-ripple to="/tram" id="tramOption">
         <q-item-section avatar>
-           <img src="marker-tram.png" width="32" height="32" />
+          <img src="marker-tram.png" width="32" height="32" />
         </q-item-section>
         <q-item-section>Tranvía</q-item-section>
-         <q-item-section side>
+        <q-item-section side>
           <q-icon name="arrow_forward" color="grey" />
         </q-item-section>
       </q-item>
 
       <q-item clickable v-ripple to="/taxi" id="taxiOption">
         <q-item-section avatar>
-           <img src="marker-taxis.png" width="32" height="32" />
+          <img src="marker-taxis.png" width="32" height="32" />
         </q-item-section>
         <q-item-section>Taxi</q-item-section>
-         <q-item-section side>
+        <q-item-section side>
           <q-icon name="arrow_forward" color="grey" />
         </q-item-section>
       </q-item>
     </q-list>
-    
+
     <q-separator />
 
     <q-list separator>
       <q-item clickable v-ripple to="/favorites" id="favoritesOption">
         <q-item-section avatar>
-           <img src="marker-favorite.png" width="32" height="32" />
+          <img src="marker-favorite.png" width="32" height="32" />
         </q-item-section>
         <q-item-section>Favoritos</q-item-section>
-         <q-item-section side>
+        <q-item-section side>
           <q-icon name="arrow_forward" color="grey" />
         </q-item-section>
       </q-item>
